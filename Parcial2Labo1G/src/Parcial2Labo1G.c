baf3e4c9b94ae8370db392132efed6cbe50510c1
@castabruno92 <castabruno92@gmail.com>
2022-11-24 21:01:21 -0300

    priemr commit

---
+++ Parcial2Labo1G/src/Parcial2Labo1G.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Parcial2Labo1G.c
+ Author      : Bruno Castagnola 1G
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
